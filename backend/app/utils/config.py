6b188f950509b74c0e308efa86c2c6cd8d605494
datmeo2k4 <datmieu2004@gmail.com>
2025-08-18 14:19:41 +0700

    chore(utils): update config api key

--- backend/app/utils/config.py
+++ backend/app/utils/config.py
@@ -2,6 +2,8 @@
 
 import os
 import re
+import itertools
+
 from urllib.parse import urlparse, parse_qs
 from pathlib import Path
 from typing import Dict
@@ -16,7 +18,7 @@ DEFAULT_CONFIG = {
     "youtube_captions": True,
     "transcription_method": "Cloud Whisper",
     "language": "auto",
-    "prompt_type": "Questions and answers",
+    # "prompt_type": "Questions and answers",
     "chunk_size": 5000,
     "parallel_api_calls": 10,
     "max_output_tokens": 4096,
@@ -33,38 +35,56 @@ GOOGLE_API_KEYS = [key for key in (
     os.environ.get("GOOGLEAI_API_KEY_4"),
 ) if key]
 
+GROQ_API_KEYS = [key for key in (
+    os.environ.get("GROQ_API_KEY"),
+    os.environ.get("GROQ_API_KEY_1"),
+    os.environ.get("GROQ_API_KEY_2"),
+) if key]
+
+class APIKeys:
+    def __init__(self, keys: list = GROQ_API_KEYS):
+        self.keys = keys
+        self.key_selector = itertools.cycle(keys)
+
+    def get_key(self) -> str:
+        if not self.keys:
+            raise ValueError("No API keys available.")
+        return next(self.key_selector)
+
 def get_api_key(config: Dict, name_api_key: str) -> str:
     """
     Get the API key from environment variables or configuration.
-
-    Args:
-        config (Dict): Configuration dictionary.
-        name_api_key (str): Name of the environment variable to look for the API key.
-    Returns:
-        str: The API key.
-    Raises:
-        ValueError: If the API key is not found in the environment variables or configuration.
     """
     if not name_api_key:
         name_api_key = "GROQ_API_KEY"
-
-    api_key = os.getenv(name_api_key)
-
-    if not api_key and "api_key" in config:
-        api_key = config["api_key"]
+    
+    api_key = APIKeys().get_key()
 
     if not api_key:
         raise ValueError("Not found API key. Please set the environment variable or provide it in the configuration.")
     print(f"Using API key from {name_api_key} environment variable or configuration.")
     return api_key
 
+# def get_api_key(config: Dict, name_api_key: str) -> str:
+#     """
+#     Get the API key from environment variables or configuration.
+#     """
+#     if not name_api_key:
+#         name_api_key = "GROQ_API_KEY"
+
+#     api_key = os.getenv(name_api_key)
+
+#     if not api_key and "api_key" in config:
+#         api_key = config["api_key"]
+
+#     if not api_key:
+#         raise ValueError("Not found API key. Please set the environment variable or provide it in the configuration.")
+#     print(f"Using API key from {name_api_key} environment variable or configuration.")
+#     return api_key
+
 def get_config(user_config: Dict) -> Dict:
     """
     Combine user-provided configuration with default settings.
-    Args:
-        user_config (Dict): User-provided configuration dictionary.
-    Returns:
-        Dict: Merged configuration dictionary.
     """
     config = DEFAULT_CONFIG.copy()
     config.update(user_config)
@@ -74,10 +94,6 @@ def get_config(user_config: Dict) -> Dict:
 def get_output_path(config: Dict) -> str:
     """
     Generate the output path for the transcript file based on the source URL or path.
-    Args:
-        config (Dict): Configuration dictionary containing the source URL or path.
-    Returns:
-        str: The output path for the transcript file.
     """
     source = config.get("source_url_or_path", "")
     source_type = config.get("type_of_source", "").lower()
